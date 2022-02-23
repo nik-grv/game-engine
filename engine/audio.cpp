b4c791800daa65e49f9d7c92d247f397f99d300f
James Freeman <p2416288@my365.dmu.ac.uk>
2022-02-23 10:26:03 +0000

    ..

---
+++ engine/audio.cpp
@@ -0,0 +1,4 @@
+/* \file audio.cpp */
+
+#include "engine_pch.h"
+#include "audio.h"
---
+++ engine/audio.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "core/application.h"
+
+namespace Engine
+{
+	class PlayableSoundComponent
+	{
+	public:
+		PlayableSoundComponent(const char* soundFilePath, bool looping = true);
+		void play(glm::vec3& location);
+		void pause();
+
+		const char* soundName;
+		int32_t channel;
+		bool isPlaying;
+		bool setToLoop;
+	};
+
+	class ParametricSoundComponent
+	{
+	public:
+		ParametricSoundComponent(const char* BankFilePath, const char* BankStringsFilePath, const char* eventName);
+		std::string eventName;
+	};
+
+	class CurrentListenerComponent
+	{
+	public:
+		CurrentListenerComponent() {}
+	};
+
+	class AudioOcculsionComponent
+	{
+	public:
+		AudioOcculsionComponent(const char* Pname, const AudioGeometryDefinition& geometryDef);
+		const char* name;
+	};
+
+	namespace AudioSystem {
+		void updatePositions();
+	}
+}
