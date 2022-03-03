bedaf47d924783eee5528617a95d141a7088eb54
Az <p2432625@GWHXX632WIN008W>
2022-03-03 16:58:57 +0000

    TG-47-added barrel rotation. TODO : clamp rotation, resolve forward vector for fire point

--- sandbox/include/behaviorScripts/tankController.h
+++ sandbox/include/behaviorScripts/tankController.h
@@ -12,7 +12,7 @@ public:
 		m_movementSpeed(movementSpeed),
 		m_active(active)
 	{
-	
+
 		entt::registry& registry = Application::getInstance().m_registry;
 		Loader::ASSIMPLoad("./assets/models/Tank/shell.obj", aiProcess_FlipUVs | aiProcess_CalcTangentSpace | aiProcess_Triangulate);
 
@@ -26,7 +26,7 @@ public:
 		std::shared_ptr<ShaderRend> shader = registry.get<RenderComponent>(entity).m_mat->getShader();
 
 		shellMat.reset(new Material(shader, Loader::output.diffusTex, glm::vec4(.5f)));
-	
+
 	};
 
 	virtual void OnUpdate(float time)
@@ -68,7 +68,7 @@ public:
 		float rotForceY = mass * (deltaRotV / time);
 
 		rp3d::Vector3 deltaV = desSpeed - rb.m_body->getLinearVelocity();
-		rp3d::Vector3 moveForce =  (deltaV / time) * mass;
+		rp3d::Vector3 moveForce = (deltaV / time) * mass;
 
 		rb.m_body->applyTorque(rp3d::Vector3(0.f, rotForceY, 0.f));
 		rb.m_body->applyForceToCenterOfMass(moveForce);
@@ -77,7 +77,7 @@ public:
 	}
 
 
-	virtual void OnMouseBtnPressed(MouseButtonPressedEvent& e) 
+	virtual void OnMouseBtnPressed(MouseButtonPressedEvent& e)
 	{
 		//projectle shooting....
 
@@ -105,11 +105,11 @@ public:
 			//auto camTransform = glm::inverse(m_camera.getCameraViewMatrix());
 			auto fireTransform = registry.get<TransformComponent>(m_entities[5]);
 			auto fireTransformMatrix = registry.get<TransformComponent>(m_entities[5]).GetTransform();
-			
+
 			//glm::mat4 inverted = glm::inverse(fireTransformMatrix);
 			glm::vec3 forward = glm::vec3(fireTransformMatrix[2][0], fireTransformMatrix[2][1], fireTransformMatrix[2][2]);
 			glm::vec3 camPos = { fireTransformMatrix[3][0],fireTransformMatrix[3][1] ,fireTransformMatrix[3][2] };
-			
+
 			auto projTransform = registry.emplace<TransformComponent>(projectileEntity, camPos - forward * 1.0f, glm::vec3(0.0f), glm::vec3(0.25f));
 
 			registry.emplace<RenderComponent>(projectileEntity, m_shellVAO, shellMat);
@@ -122,12 +122,12 @@ public:
 			projectiel_rb.m_body->applyForceToCenterOfMass(rp3d::Vector3(force.x, force.y + 20.f, force.z - 1500.f));
 		}
 	}
-	
+
 	virtual void OnMouseMoved(MouseMovedEvent& e)
 	{
 		entt::registry& registry = Application::getInstance().m_registry;
-		auto& m_tankHead = registry.get<RigidBodyComponent>(HierarchySystem::GetChildEntity(m_entity,0));
-		//auto& m_tankBarrel = registry.get<RigidBodyComponent>(HierarchySystem::GetChildEntity(m_entity,1));
+		auto& m_tankHead = registry.get<RigidBodyComponent>(HierarchySystem::GetChildEntity(m_entity, 0));
+		auto& m_tankBarrel = registry.get<RigidBodyComponent>(HierarchySystem::GetChildEntity(HierarchySystem::GetChildEntity(m_entity, 0), 0));
 		float xpos = e.getX();
 		float ypos = e.getY();
 
@@ -159,38 +159,49 @@ public:
 		front.x = cos(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
 		front.y = sin(glm::radians(m_pitch));
 		front.z = sin(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
-		//m_cameraFront = glm::normalize(front);
 
-		//glm::rotate(e.getMousePos())
 		if (xoffset > 0.5f)
 		{
 			m_tankHead.m_body->setAngularVelocity(rp3d::Vector3(0, -1.0f, 0.0f));
-			//m_tankBarrel.m_body->setAngularVelocity(rp3d::Vector3(0, -1.0f, 0.0f));
 			m_tankHead.m_body->setAngularDamping(0.9f);
-		//	m_tankBarrel.m_body->setAngularDamping(0.9f);
-			//m_tankRB.m_body->applyTorque(-rp3d::Vector3(0.0f, xoffset, 0.0f));
 		}
 		else if (xoffset < -0.5f)
 		{
 			m_tankHead.m_body->setAngularVelocity(rp3d::Vector3(0, 1.0f, 0.0f));
-			//m_tankBarrel.m_body->setAngularVelocity(rp3d::Vector3(0, 1.0f, 0.0f));
 			m_tankHead.m_body->setAngularDamping(0.9f);
-			//m_tankBarrel.m_body->setAngularDamping(0.9f);
-			//Log::error("MOUSE = {0}", xoffset);
-			//m_tankRB.m_body->applyTorque(rp3d::Vector3(0.0f, -xoffset, 0.0f));
 		}
-		
-		if (yoffset > 0.5f)
+
+
+		if (yoffset > 0.8f)
 		{
-		//	m_tankBarrel.m_body->setAngularVelocity(rp3d::Vector3(0.5f, 0.0f, 0.0f));
-		//	m_tankBarrel.m_body->setAngularDamping(0.9f);
+			m_tankBarrel.m_body->setAngularVelocity(rp3d::Vector3(0.5f, 0.0f, 0.0f));
+			m_tankBarrel.m_body->setAngularDamping(0.9f);
 
 		}
-		else if (yoffset < -0.5f)
+		else if (yoffset < -0.8f)
 		{
-			//m_tankBarrel.m_body->setAngularVelocity(rp3d::Vector3(-0.5f, 0.0f, 0.0f));
-			//m_tankBarrel.m_body->setAngularDamping(0.9f);
+			m_tankBarrel.m_body->setAngularVelocity(rp3d::Vector3(-0.5f, 0.0f, 0.0f));
+			m_tankBarrel.m_body->setAngularDamping(0.9f);
 		}
+		rp3d::Transform t = m_tankBarrel.m_body->getTransform();
+
+
+		if (m_tankBarrel.m_body->getTransform().getOrientation().x > 0.12f)
+		{
+			Log::error("Clamping,,...");
+			rp3d::Vector3 ve = { 0.12f,0,0 };
+			rp3d::Quaternion quat = { 0,ve };
+			t.setOrientation(quat);
+		}
+		else if (m_tankBarrel.m_body->getTransform().getOrientation().x < 0.000f)
+		{
+			rp3d::Vector3 ve = { 0,0,0 };
+			rp3d::Quaternion quat = { ve, 0 };
+			t.setOrientation(quat);
+		}
+
+		Log::warn(m_tankBarrel.m_body->getTransform().getOrientation().x);
+
 	}
 
 	virtual void OnKeyPressed(KeyPressedEvent& e)
