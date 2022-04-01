11f98bf7e79aa3059e5f83367fa0d7950a9eda1d
James Freeman <p2416288@my365.dmu.ac.uk>
2022-04-01 13:30:29 +0100

    added a comment

--- engine/enginecode/include/independent/core/application.h
+++ engine/enginecode/include/independent/core/application.h
@@ -56,7 +56,7 @@ namespace Engine {
 
 		EventHandler m_eventHandler; //!< Event handler
 
-		std::shared_ptr< AudioManager> m_audio;
+		std::shared_ptr< AudioManager> m_audio;//!< audio manager
 		
 		
 		bool onWindowClose(WindowCloseEvent& e); //!<run when window closes
